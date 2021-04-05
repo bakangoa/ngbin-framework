1482f083009dd5033c2dc94d6471dc635570f67b
Nanok Charles <konancharlesyao@gmail.com>
2021-04-05 23:37:49 +0000

    Basic example set

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 
     require_once "app/autoload.php";
 
-    Goa\Ngbinfwk\Loader::loadFiles([
-        "controllers"
-    ]);
-
     $app = new \Ngbin\Framework\App();
 
     $app->addGetRoute("/ngbin-framework", "", function () {
-        return "Hello Nanok";
+        return "Hello World";
     });
 
     $app->run();
